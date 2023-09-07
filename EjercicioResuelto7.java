947a61e21310fa62264da326b01f58cc0411cc25
Jhony Alpala <jhoniher95@gmail.com>
2023-09-07 10:25:13 -0500

    Condiciones establecidas

--- EjercicioResuelto7.java
+++ EjercicioResuelto7.java
@@ -1,2 +1,19 @@
+import java.util.Scanner;
 public class EjercicioResuelto7 {
+    public static void main(String[] args) {
+        Scanner entrada=new Scanner(System.in);
+        int a,b;
+        System.out.println("Ingrese el primer numero: ");
+        a=entrada.nextInt();
+        System.out.println("Ingrese el segundo numero: ");
+        b= entrada.nextInt();
+        if(a>b){
+            System.out.println(a+" es mayor que "+b);
+        } else if (a<b) {
+            System.out.printf(a+" es menor que "+b);
+        }
+        else{
+            System.out.println(a+" es igual que "+b);
+        }
+    }
 }
