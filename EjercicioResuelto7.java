80128a64b0b79f872dfe090a1e8457aab6cb9b56
Jhony Alpala <jhoniher95@gmail.com>
2023-09-07 10:27:45 -0500

    Ejercicio finalizado

--- EjercicioResuelto7.java
+++ EjercicioResuelto7.java
@@ -1,18 +1,19 @@
+//Determinar si un numero es mayor, menor o igual que otro
 import java.util.Scanner;
 public class EjercicioResuelto7 {
     public static void main(String[] args) {
         Scanner entrada=new Scanner(System.in);
-        int a,b;
+        int a,b; //Declaracion de variables
         System.out.println("Ingrese el primer numero: ");
-        a=entrada.nextInt();
+        a=entrada.nextInt(); //Entrada del primer numero
         System.out.println("Ingrese el segundo numero: ");
-        b= entrada.nextInt();
-        if(a>b){
+        b= entrada.nextInt(); //Entrada del segundo numero
+        if(a>b){ //Condicional, pregunta si es mayor
             System.out.println(a+" es mayor que "+b);
-        } else if (a<b) {
+        } else if (a<b) { //Condicional, pregunta se es menor
             System.out.printf(a+" es menor que "+b);
         }
-        else{
+        else{ //Si no se cumplen los casos, entonces son iguales
             System.out.println(a+" es igual que "+b);
         }
     }
